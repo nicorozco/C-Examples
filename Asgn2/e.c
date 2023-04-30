afa2aaf1a83cb039e40a1b843147533296e76324
norozco6 <114160450+norozco6@users.noreply.github.com>
2023-04-30 12:36:57 -0700

    works just going to make sure it works 100%

--- Asgn2/e.c
+++ Asgn2/e.c
@@ -1,17 +1,33 @@
-//defining static variable iters
-static int inters = 0;
-//defining epsilon 
+#include <stdio.h>
 #define EPSILON 1e-14
+#include "mathlib.h"
+//defining static variable iters
+static int iters = 0;
 // contains the functions: e() and e_terms()
 // e() will compute the approximation of e and track the number of computed terms by static variable local to file
-long double e()
+double e(void)
 {
- long double numerator = 1.0
- long double denominator = 1.0 
- while ((fabs(
- 
+		int euler = 1;
+		iters = 0;
+		double k = 1;
+		double numerator = 1.0;
+ 		double denominator = 1.0; 
+ 		double sum = numerator/denominator;
 
+	while (euler == 1)
+	{
+		double term = numerator / denominator;
+		if ((absolute(term)) < EPSILON)
+		{ 
+			euler = 0;
+		}
+		denominator *= k + 1;
+		k +=1;
+		iters +=1;
+		sum += term;
 
+	}
+	return sum;
 }
 
 
@@ -25,7 +41,12 @@ int e_terms(void)
 
 
 }
-~                                                                                                                                   
-~                                                                                                                                   
-~                                                                                                                      
-~                                                                   
+
+int main(void)
+{
+
+
+	printf("This is the value of e: %f\n",e());
+	printf("This is the value of iters: %d\n",e_terms());
+	
+}
